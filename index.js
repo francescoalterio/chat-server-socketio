2ee2afcaf54ba9ed4ba7d8171a22c2f07cf2b408
francescoalterio <francescoalteriog@gmail.com>
2022-04-12 12:19:49 -0400

    with name

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
+  console.log("usuario conectado");
   socket.on("chat message", (msg) => {
+    console.log(msg);
     io.emit("chat message", msg);
   });
 });
